d60c6ec337bf429a003fbd8c8e796c6a71f61acd
Kumar Anuj <131229689+seneva981@users.noreply.github.com>
2024-10-06 13:34:44 +0530

    Add files via upload

---
+++ Problem_02.c
@@ -0,0 +1,18 @@
+// Variables
+
+#include <stdio.h>
+int main(){
+    int number = 25;
+    char star = '*';
+    int age = 22;
+    age = 24;
+    float pi = 3.14;
+
+    int a = 23;
+    int A = 40;
+
+    int _age = 22;
+
+    int final_price = 100;
+    return 0;
+}
